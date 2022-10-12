3978918cdce9e693d6843716268effa8ba3be7bf
akashp69 <akashpakhare499@gmail.com>
2022-10-12 20:29:38 +0530

    Added:Functional Programs

---
+++ src/com/bridgelabz/functionalprograms/Distance.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.functionalprograms;
+
+
+    import java.io.InputStream;
+import java.util.*;
+    public class Distance {
+        public static void main(String[] args) {
+            double x1;
+            double y1;
+            System.out.println("Enter the coordinates:x1,y1");
+            Scanner s  =new Scanner(System.in);
+            x1 = s.nextDouble();
+            y1 = s.nextDouble();
+            double distance = Math.sqrt((Math.pow(x1, 2) + (Math.pow(y1, 2))));
+            System.out.println("Eucledian distance between entered coordinates is = "+distance);
+        }
+    }
+
+
