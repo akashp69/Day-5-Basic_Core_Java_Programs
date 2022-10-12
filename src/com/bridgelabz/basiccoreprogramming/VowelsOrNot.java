580818f7daf3e2ce3de45e2ea8345378d4b2541a
akashp69 <akashpakhare499@gmail.com>
2022-10-12 20:35:24 +0530

    Added:Vowel or cosonant program

---
+++ src/com/bridgelabz/basiccoreprogramming/VowelsOrNot.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.basiccoreprogramming;
+
+import java.util.Scanner;
+
+public class VowelsOrNot {
+    static void checkVowel(){
+        char alphabet;
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o'| ch=='u')
+        System.out.println("Vowel");
+        else
+        System.out.println("NOT Vowel");
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Check Letter is Vowel Or NOT");
+        checkVowel();
+    }
+}
