7d6f6647576fe3166d4da1d4525102f2fc3757a9
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-10-17 14:24:03 +0530

    Update VowelsOrNot.java

--- src/com/bridgelabz/basiccoreprogramming/VowelsOrNot.java
+++ src/com/bridgelabz/basiccoreprogramming/VowelsOrNot.java
@@ -7,7 +7,7 @@ public class VowelsOrNot {
         char alphabet;
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().charAt(0);
-        if(ch=='a' || ch=='e' || ch=='i' || ch=='o'| ch=='u')
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u')
         System.out.println("Vowel");
         else
         System.out.println("NOT Vowel");
